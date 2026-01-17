16efff8c30d26b267e4d7761df45fb41231b35a5
HuyDung <nguyenhdung2006@gmail.com>
2026-01-17 21:36:37 +0700

    connect to MySql

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -7,6 +7,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 	@GetMapping("/")
 	public String hello() {
-		return "Hello World with hoidanit";
+		return "Hello World updates";
+	}
+
+	@GetMapping("/user")
+	public String userPage() {
+		return "Only user can access this page.";
+	}
+
+	@GetMapping("/admin")
+	public String adminPage() {
+		return "Only admin can access this page.";
 	}
 }
